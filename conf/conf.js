8347e294d84a41986360089e1edfb201f149205e
worminger <emumonger@yahoo.com>
2015-03-22 18:00:44 -0700

    Move fingerprint logic into separate module and test it

--- conf/conf.js
+++ conf/conf.js
@@ -3,6 +3,9 @@ var conf = {
         port: 3000
     },
     
+    // fingerprint string encoding
+    hashEncoding: 'hex',
+    
     // maximum input image size in octets
     contentLengthLimit: (1 << 20) * 3,
     
