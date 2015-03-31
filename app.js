3c140da0e3c8f2328b070e90c71e73c9fdc34608
Alaric Holloway <emumonger@yahoo.com>
2015-03-30 17:25:55 -0700

    Simplify processing logic and report errors instead of generating 500 status codes.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var conf = require('./conf/conf');
 var download = require('./lib/download');
 var fingerprint = require('./lib/fingerprint');
 var payload = require('./lib/payload');
-var au = require('./lib/util/array');
 
 var app = express();
 var server = app.listen(conf.server.port);
@@ -19,25 +18,19 @@ app.get('/', function(request, response) {
     
     try {
         var url = JSON.parse(decodeURIComponent(encodedUrl));
-    
-        // NOTE: If a single download fails, the entire request fails :(
-        async.waterfall([
-            async.apply(download.multiple, Array.isArray(url) ? url : [url]),
-            processDownloads,
-        ], async.apply(handleOk, response));
     } catch (e) {
-        handleBadRequest(response);
+        return handleBadRequest(response);
     }
+    
+    async.map(
+        Array.isArray(url) ? url : [url],
+        process,
+        async.apply(handleOk, response)
+    );
 });
 
 function handleOk(response, error, results) {
-    if (error) {
-        response
-            .status(500)
-            .send('Error: ' + error);
-    } else {
-        response.send(payload.asJSON(results));
-    }
+    response.send(payload.asJSON(results));
 }
 
 function handleBadRequest(response) {
@@ -46,21 +39,23 @@ function handleBadRequest(response) {
         .send('Bad Request');
 }
 
-function processDownloads(fileInfos, callback) {
-    var filePaths = au.extract(fileInfos, 'path');
-    fingerprint.deriveMultiple(filePaths, function(error, hashes) {
+function process(url, callback) {
+    async.waterfall([
+        async.apply(download.image, url),
+        processDownload
+    ], function(error, result) {
+        if (error) return callback(null, "Error: " + error);
+        callback(null, result);
+    });
+}
+
+function processDownload(fileInfo, callback) {
+    fingerprint.derive(fileInfo.path, function(error, hash) {
         if (error) return callback(error);
-        
-        var results = [];
-        for (var i = 0; i < fileInfos.length; i++) {
-            var info = {
-                hash: hashes[i],
-                headers: fileInfos[i].headers
-            };
-            results.push(info);
-        }
-        
-        callback(null, results);
+        callback(null, {
+            hash: hash,
+            headers: fileInfo.headers
+        });
     });
 }
 
